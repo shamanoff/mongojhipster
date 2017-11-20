0658a79fb95ffd8c6cc70671d59bd5ed6a944a6c
Shamanoff <shamanoff@gmail.com>
2017-11-20 15:00:54 +0200

    work

---
+++ src/main/java/mongojh/repository/TaskRepository.java
@@ -0,0 +1,15 @@
+package mongojh.repository;
+
+import mongojh.domain.Task;
+import org.springframework.stereotype.Repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Spring Data MongoDB repository for the Task entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface TaskRepository extends MongoRepository<Task, String> {
+
+}
